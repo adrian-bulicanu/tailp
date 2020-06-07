c64688eaed73c258ed190261ceb3345a99df6e9b
Adrian Bulicanu <adrian.bulicanu@gmail.com>
2020-06-07 14:02:38 +0300

    ! added unit tests

---
+++ tailpTests/TailPblTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TailP;
+using System;
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
+
+namespace tailpTests
+{
+    [TestClass()]
+    public class TailPblTests
+    {
+        private TailPbl _fixture;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fixture = new TailPbl((l, i) => { });
+        }
+
+        [TestMethod()]
+        public void ParseArgsTest01()
+        {
+            try
+            {
+                _fixture.ParseArgs(new string[0]);
+                Assert.Fail("An exception should have been thrown");
+            }
+            catch (TailPException ex)
+            {
+                Assert.AreEqual("Invalid args", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Unexpected exception of type {ex.GetType()} caught: {ex.Message}");
+            }
+        }
+
+        [TestMethod()]
+        public void ParseArgsTest02()
+        {
+            _fixture.ParseArgs(new[] { @"-f", @"dummy_file" });
+            var act = Configuration.Follow;
+            var exp = true;
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ParseArgsTest03()
+        {
+            _fixture.ParseArgs(new[] { @"--logical-lines", @"<<<", @"dummy_file" });
+            var act = Configuration.LogicalLineMarker;
+            var exp = @"<<<";
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void GetHelpTest()
+        {
+            var act = _fixture.GetHelp();
+            Assert.AreNotEqual(string.Empty, act);
+        }
+    }
+}
---
+++ tailpTests/TimespanExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TailP;
+using System;
+// ReSharper disable ConvertToConstant.Local
+
+namespace tailpTests
+{
+    [TestClass()]
+    public class TimespanExtensionsTests
+    {
+        [TestMethod()]
+        public void ToHumanReadableStringTest01()
+        {
+            var act = TimeSpan.Zero.ToHumanReadableString();
+            var exp = @"0 second(s)";
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ToHumanReadableStringTest02()
+        {
+            var act = TimeSpan.FromSeconds(5).ToHumanReadableString();
+            var exp = @"5 second(s)";
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ToHumanReadableStringTest03()
+        {
+            var act = TimeSpan.FromSeconds(2 * 60 + 3).ToHumanReadableString();
+            var exp = @"2:03 minute(s)";
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ToHumanReadableStringTest04()
+        {
+            var act = TimeSpan.FromSeconds(4 * 3600 + 2 * 60 + 3).ToHumanReadableString();
+            var exp = @"4:02 hour(s)";
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ToHumanReadableStringTest05()
+        {
+            var act = TimeSpan.FromSeconds(24* 3600 + 4 * 3600 + 2 * 60 + 3).ToHumanReadableString();
+            var exp = @"over 28 hour(s)";
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ToHumanReadableStringTest06()
+        {
+            var act = TimeSpan.FromSeconds(48 * 3600 + 4 * 3600 + 2 * 60 + 3).ToHumanReadableString();
+            var exp = @"over 2 day(s)";
+            Assert.AreEqual(exp, act);
+        }
+    }
+}
